c41494e094c961b0c3b56719ebbe006c5d8d2f67
Sabrine Dhaliwal <sabrine.dhaliwal@gmail.com>
2023-09-05 19:42:10 -0700

    getting single summary, put functioning

--- controllers/summaryController.js
+++ controllers/summaryController.js
@@ -1,97 +1,170 @@
-const knex = require('knex')(require("../knexfile"));
+const knex = require("knex")(require("../knexfile"));
 
-const addNewSummary = (req, res)=> {
+const addNewSummary = (req, res) => {
+  //initial backend validation
+  if (
+    !req.body.date ||
+    !req.body.id ||
+    !req.body.position ||
+    !req.body.target ||
+    !req.body.tech ||
+    !req.body.title
+  ) {
+    return res.status(400).json({ message: "Please fill in required field" });
+  } else {
+    const positionsOptionsValue = req.body.position;
+    const [position_id, position_name] = positionsOptionsValue.split(",");
+    const targetOptionsValue = req.body.target;
+    const [target_id, target_name] = targetOptionsValue.split(",");
+    const techOptionsValue = req.body.tech;
+    const [tech_id, tech_name] = techOptionsValue.split(",");
 
-    //initial backend validation
-    console.log('file', req.file);
-    // console.log(req.body);
-    if (!req.body.date ||
-        !req.body.id ||
-        !req.body.position ||
-        !req.body.target ||
-        !req.body.tech ||
-        !req.body.title
-        ){
-            return res.status(400).json({message: "Please fill in required field"})
-        }else {
-            const positionsOptionsValue = req.body.position;
-            const [position_id, position_name] = positionsOptionsValue.split(",");
-            const targetOptionsValue = req.body.target;
-            const [target_id, target_name] = targetOptionsValue.split(",");
-            const techOptionsValue = req.body.tech;
-            const [tech_id, tech_name] = techOptionsValue.split(",");
-
-            knex('summaries')
-            .insert({
-                date: req.body.date,
-                user_id: req.body.id,
-                position_id: Number(position_id),
-                position_name: position_name,
-                target_id: Number(target_id),
-                target_name: target_name,
-                tech_id: Number(tech_id),
-                tech_name: tech_name,
-                title: req.body.title,
-                summary:req.body.summary,
-                video: req.file.path
-
-            }) .then(()=>{
-                res.status(201).json({message:"new record added successfully"})
-            
-            })
-            .catch((err)=> {
-                console.error(err, "If you can read this, something went wrong at summary controller")
-                res.status(500).json({message:"internal server error at summary controller"})
-            })
-        } 
-}
-
-const getSummary =(req, res)=> {
     knex("summaries")
+      .insert({
+        date: req.body.date,
+        user_id: req.body.id,
+        position_id: Number(position_id),
+        position_name: position_name,
+        target_id: Number(target_id),
+        target_name: target_name,
+        tech_id: Number(tech_id),
+        tech_name: tech_name,
+        title: req.body.title,
+        summary: req.body.summary,
+        video: req.file.path,
+      })
+      .then(() => {
+        res.status(201).json({ message: "new record added successfully" });
+      })
+      .catch((err) => {
+        console.error(
+          err,
+          "If you can read this, something went wrong at summary controller"
+        );
+        res
+          .status(500)
+          .json({ message: "internal server error at summary controller" });
+      });
+  }
+};
+
+const getSummaries = (req, res) => {
+  knex("summaries")
     .select("*")
-    .where({user_id: Number(req.params.id)})
-    .then((summary)=> {
-        res.status(200).json({summary, message:"successful get query, CONGRATS!"});
+    .orderBy("date","desc")
+    .where({ user_id: Number(req.params.id) })
+    .then((summary) => {
+      res
+        .status(200)
+        .json({ summary, message: "successful get query, CONGRATS!" });
     })
-    .catch((err)=> {
-        console.error(err, "if you can see this, something went wrong at getSummary call at summary controller")
-        res.status(500).json({message:"internal server error at getSummary query- summary controller"})
-    })
-}
+    .catch((err) => {
+      console.error(
+        err,
+        "if you can see this, something went wrong at getSummary call at summary controller"
+      );
+      res
+        .status(500)
+        .json({
+          message:
+            "internal server error at getSummary query- summary controller",
+        });
+    });
+};
 
-const deleteSummary =(req, res)=> {
-    console.log(req.params)
-    knex("summaries")
+const deleteSummary = (req, res) => {
+  console.log(req.params);
+  knex("summaries")
     .where("summary_id", req.params.summary_id)
     .select("summary_id")
-    .del()
-    .then((data)=> {
-        res.status(204).send("successfully deleted")
+    // .del()
+    .then((data) => {
+      res.status(204).send("successfully deleted");
     })
-    .catch((err)=> {
-        console.log(err)
-        res.status(500).send("Could not delete entry, its on the internet forever")
-    })
-}
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send("Could not delete entry, its on the internet forever");
+    });
+};
 
-const editSummary = (req, res) => {
-    console.log(req.params)
-    knex('summaries')
-    .where("summary_id", req.params.summary_id)
-    .select('summary_id')
-    .update()
-    .then((data)=> {
-        console.log(data)
-        res.status(202).send("successfully edited summary")
+//editSummary logic
+const getSummary = (req, res) => {
+  // console.log(req.params);
+  knex("summaries")
+    .where("summary_id", req.params.summaryid)
+    .select("*")
+    .then((getSummariesResponse) => {
+      if (getSummariesResponse.length === 0){
+        return res.status(404).sned("Summary not found")
+      }
+      const getSummary ={ 
+        date: getSummariesResponse[0].date,
+        tech_name: getSummariesResponse[0].tech_name,
+        target_name: getSummariesResponse[0].target_name,
+        position_name: getSummariesResponse[0].position_name,
+        summary:getSummariesResponse[0].summary,
+        title: getSummariesResponse[0].title,
+        user_id:getSummariesResponse[0].user_id,
+      }
+      res.status(202).send(getSummary);
+      console.log("get summary condenses", getSummary)
     })
-    .catch((err)=> {
-        console.error(err, "something went wrong at ediySummary summaryController")
-        res.status(500).send("Internal error could not edit summary- check summary controller")
-    })
+
+    .catch((err) => {
+      console.error(
+        err,
+        "something went wrong at editSummary summaryController"
+      );
+      res
+        .status(500)
+        .send(
+          "Internal error could not edit summary- check summary controller"
+        );
+    });
+};
+
+
+const updateSummary=(req, res)=> {
+  console.log(req.body)
+    const positionsOptionsValue = req.body.position_name;
+    const [position_id, position_name] = positionsOptionsValue.split(",");
+    const targetOptionsValue = req.body.target_name;
+    const [target_id, target_name] = targetOptionsValue.split(",");
+    const techOptionsValue = req.body.tech_name;
+    const [tech_id, tech_name] = techOptionsValue.split(",");
+
+    knex("summaries")
+    .where('summary_id', req.params.summaryid)
+    .select('*')
+    .update({
+      date: req.body.date,
+        user_id: req.body.id,
+        position_id: Number(position_id),
+        position_name: position_name,
+        target_id: Number(target_id),
+        target_name: target_name,
+        tech_id: Number(tech_id),
+        tech_name: tech_name,
+        title: req.body.title,
+        summary: req.body.summary,
+})
+.then((response)=> {
+  console.log("successful?", response)
+  res.status(202).send(response)
+})
+.catch((err)=>{
+  console.error(err)
+})
 }
-module.exports= {
-    addNewSummary,
-    getSummary,
-    deleteSummary,
-    editSummary,
-}
+  
+
+
+module.exports = {
+  addNewSummary,
+  getSummaries,
+  deleteSummary,
+  getSummary,
+  updateSummary,
+};
--- routes/summaryRoutes.js
+++ routes/summaryRoutes.js
@@ -1,38 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-// const ffmpeg = require("fluent-ffmpeg");
-
-// const thumbnail= (req, res, next) =>{
-//   console.log("thumbnail file", req.file)
-//   ffmpeg("./uploads")
-//   .screenshots({
-//     timestamps: ['10%'], // Capture a thumbnail at 10% of the video duration
-//     folder: "./thumbnail",
-//     filename: req.file.filename.replace('.mp4', '.png'),
-//     size: '320x240', // Adjust the size as needed
-//   })
-//   .on('end', () => {
-//     console.log(`Thumbnail created for ${req.file.filename}`);
-//   })
-//   .on('error', (err) => {
-//     console.error(`Error generating thumbnail for ${req.file.filename}: ${err}`);
-//   });
-//   next()
-// } 
-
-// const thumbnail =(req, res, next)=> {
-//   console.log(req.file, "req.file sharp")
-//   sharp(req.file.destination).resize(200, 200).toFile('uploads/' + 'thumbnails-' + req.file.originalname, (err, resizeImage) => {
-//     if (err) {
-//          console.log(err);
-//     } else {
-//          console.log(resizeImage);
-//     }
-// }); 
-// next();
-// }
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -51,8 +19,8 @@ const upload = multer({ storage: storage });
 const summaryController = require("../controllers/summaryController");
 
 router.route("/").post(upload.single('video'), summaryController.addNewSummary)
-router.route("/:id").get(summaryController.getSummary)
-router.route("/:id/:summary_id").delete(summaryController.deleteSummary).put(summaryController.editSummary);
+router.route("/:id").get(summaryController.getSummaries)
+router.route("/edit/:summaryid").delete(summaryController.deleteSummary).get(summaryController.getSummary).put(summaryController.updateSummary);
 
 module.exports = router;
 
