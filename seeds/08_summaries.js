d9d34c5bc9085cdd5deb18ba1a5c8f89ce1cd78b
Sabrine Dhaliwal <sabrine.dhaliwal@gmail.com>
2023-09-14 10:55:55 -0700

    cleaned up upload folder

--- seeds/08_summaries.js
+++ seeds/08_summaries.js
@@ -12,7 +12,7 @@ exports.seed = async function (knex) {
       target_name: "Shoulder",
       title: "Nailed that Armbar from side control",
       summary:
-        "Today we worked on landing armbars from different sidecontrol situations. How to transition from kimura to armbar",
+        "I focused on making sure I was shifting my weight across my partners chest and using my near head sid leg to control them.",
       video: "uploads/2023-09-05T17-04-29.099Z-video2.mp4",
       date: "2023-07-10",
     },
@@ -25,9 +25,9 @@ exports.seed = async function (knex) {
       position_name: "North South",
       target_id:4,
       target_name: "Ankle",
-      title: "Nailed that Armbar from side control",
+      title: "Ankel locks from North South?",
       summary:
-        "Today we worked on landing armbars from different sidecontrol situations. How to transition from kimura to armbar",
+        "Not totally impossible but you gotta be quick to pull yourself from noth south to underthem and grab oneof their ankles and trap it in your armpit ",
       video: "uploads/2023-09-05T17-04-29.099Z-video2.mp4",
       date: "2023-07-10",
     },
@@ -42,7 +42,7 @@ exports.seed = async function (knex) {
       target_name: "Shoulder",
       title: "Nailed that Armbar from side control",
       summary:
-        "Today we worked on landing armbars from different sidecontrol situations. How to transition from kimura to armbar",
+        " transtioning from side control to pulling them upwards and almost riding them like in a saddle to finish the kimura",
       video: "uploads/2023-09-05T17-04-29.099Z-video2.mp4",
       date: "2023-07-10",
     },
@@ -76,20 +76,6 @@ exports.seed = async function (knex) {
       video: "uploads/2023-09-05T17-04-29.099Z-video2.mp4",
       date: "2023-07-10",
     },
-       {
-      summary_id: 6,
-      user_id: 1,
-      tech_id: 3,
-      tech_name:"Kimura",
-      position_id: 1,
-      position_name: "Side Control",
-      target_id:2,
-      target_name: "Shoulder",
-      title: "Nailed that Armbar from side control",
-      summary:
-        "Today we worked on landing armbars from different sidecontrol situations. How to transition from kimura to armbar",
-      video: "uploads/2023-09-05T17-04-29.099Z-video2.mp4",
-      date: "2023-07-10",
-    },
+    
   ]);
 };
